b0c5e09f722208ff8af57b94b81872a96069f9b7
baimhons <poonapop.s@gmail.com>
2025-10-26 01:49:49 +0700

    CORS

--- backend/internal/middlewares/configs/cors.go
+++ backend/internal/middlewares/configs/cors.go
@@ -9,7 +9,7 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://202.44.40.193", "http://localhost:5500", "https://stadiumhub-1.onrender.com/"},
+		AllowOrigins:     []string{"http://202.44.40.193", "http://localhost:5500", "https://stadiumhub-1.onrender.com"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"},
 		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
